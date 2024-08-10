c14823c2505627804f91c517fecf4db7123969e7
Witoon Aunsaen <witoon1222@gmail.com>
2024-08-10 10:50:47 +0700

    first commit

---
+++ lib/screen/home/home_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: const Center(
+        child: Text('Home Screen'),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
